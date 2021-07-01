d719a2cba52d09cd97f2953bebcdebe3e4cb102e
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-07-01 10:06:14 -0300

    Profile dev

--- backend/src/main/java/com/devsuperior/dspesquisa/controllers/RecordController.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/controllers/RecordController.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,17 +22,17 @@ import com.devsuperior.dspesquisa.services.RecordService;
 @RestController
 @RequestMapping("/records")
 public class RecordController {
-	
+
 	@Autowired
 	private RecordService service;
-	
+
 	@PostMapping
 	public ResponseEntity<RecordDto> insert(@RequestBody RecordInsertDto dto) {
 		RecordDto newDto = service.insert(dto);
 		return ResponseEntity.ok().body(newDto);
 	}
-	
-	@GetMapping
+
+	@GetMapping("/by-moment")
 	public ResponseEntity<Page<RecordDto>> findByMoment(
 			@RequestParam(value = "min", defaultValue = "moment") String min,
 			@RequestParam(value = "max", defaultValue = "moment") String max,
@@ -39,17 +40,36 @@ public class RecordController {
 			@RequestParam(value = "linesPerPage", defaultValue = "0") Integer linesPerPage,
 			@RequestParam(value = "orderBy", defaultValue = "moment") String orderBy,
 			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
-		
-		Instant minDate = ("".equals(min)) ? null : Instant.parse(min);
-		Instant maxDate = ("".equals(max)) ? null : Instant.parse(max);
-		
+
 		if (linesPerPage == 0) {
 			linesPerPage = Integer.MAX_VALUE;
 		}
-		
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		
+
+		Pageable pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		Instant minDate = ("".equals(min)) ? null : Instant.parse(min);
+		Instant maxDate = ("".equals(max)) ? null : Instant.parse(max);
+
 		Page<RecordDto> listRecords = service.findByMoment(minDate, maxDate, pageRequest);
 		return ResponseEntity.ok().body(listRecords);
+
+	}
+
+	@GetMapping
+	public ResponseEntity<Page<RecordDto>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "0") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "moment") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction) {
+
+		if (linesPerPage == 0) {
+			linesPerPage = Integer.MAX_VALUE;
+		}
+
+		Pageable pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+
+		Page<RecordDto> listRecords = service.findAll(pageRequest);
+		return ResponseEntity.ok().body(listRecords);
+
 	}
 }
--- backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/services/RecordService.java
@@ -4,7 +4,7 @@ import java.time.Instant;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,33 +17,40 @@ import com.devsuperior.dspesquisa.repositories.RecordRepository;
 
 @Service
 public class RecordService {
-	
+
 	@Autowired
 	private RecordRepository repository;
-	
+
 	@Autowired
 	private GameRepository gameRepository;
-	
+
 	@Transactional
 	public RecordDto insert(RecordInsertDto dto) {
-		
+
 		Record entity = new Record();
-		
+
 		entity.setName(dto.getName());
 		entity.setAge(dto.getAge());
 		entity.setMoment(Instant.now());
-		
+
 		Game game = gameRepository.getOne(dto.getGameId());
 		entity.setGame(game);
-		
+
 		entity = repository.save(entity);
-		
+
 		return new RecordDto(entity);
 	}
-	
+
 	@Transactional(readOnly = true)
-	public Page<RecordDto> findByMoment(Instant minDate, Instant maxDate, PageRequest pageRequest) {
-		
+	public Page<RecordDto> findByMoment(Instant minDate, Instant maxDate, Pageable pageRequest) {
+
 		return repository.findByMoment(minDate, maxDate, pageRequest).map(RecordDto::new);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<RecordDto> findAll(Pageable pageRequest) {
+
+		return repository.findAll(pageRequest).map(RecordDto::new);
+
 	}
 }
