da2e91b0b701f16f8f6972c4c8e3b2f38a704ccc
nabucodonosor-java <nabucodonosor.java@gmail.com>
2021-07-01 18:48:38 -0300

    Add Pagination component

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -1,10 +1,64 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Pagination from 'components/Pagination';
+import { RecordResponse } from './types';
+import { formtDate } from './helpers';
+import './styles.css';
+import { Link } from 'react-router-dom';
+
+const BASE_URL = 'http://localhost:8080';
 
 const Records = () => {
 
+    const [recordsRespose, setRecordsResponse] = useState<RecordResponse>();
+    const [activePage, setActivePage] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
+        .then(response => setRecordsResponse(response.data));
+    }, [activePage]);
+
+    const handlePageChange = (index: number) => {
+        setActivePage(index);
+    }
+
     return (
-        <div>
-            <h1>Records</h1>
+        <div className="page-container">
+            <div className="filters-container records-actions">
+                <Link to="/charts">
+                    <button className="action-filters">
+                        VER GRÁFICOS
+                    </button>
+                </Link>
+            </div>
+            <table className="records-table" cellPadding="0" cellSpacing="0">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>NOME</th>
+                        <th>IDADE</th>
+                        <th>PLATAFORMA</th>
+                        <th>GÊNERO</th>
+                        <th>TÍTULO DO GAME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {recordsRespose?.content.map(record => (
+                         <tr key={record.id}>
+                            <td>{formtDate(record.moment)}</td>
+                            <td>{record.name}</td>
+                            <td>{record.age}</td>
+                            <td className="text-secondary">{record.gamePlatform}</td>
+                            <td>{record.genreName}</td>
+                            <td className="text-primary">{record.gameTitle}</td>
+                        </tr>
+                    ))}    
+                </tbody>
+            </table>
+            <Pagination 
+            activePage={activePage}
+            goToPage={handlePageChange}
+            totalPages={recordsRespose?.totalPages} />               
         </div>
     );
 }
---
+++ front-web/src/pages/Records/types.ts
@@ -0,0 +1,17 @@
+export type RecordResponse = {
+    content: RecordItem[];
+    totalPages: number;
+}
+
+export type RecordItem = {
+    id: number;
+    moment: string;
+    name: string;
+    age: number;
+    gameTitle: string;
+    gamePlatform: Platform;
+    genreName: string;
+}
+
+export type Platform = 'XBOX' | 'PC' | 'PLAYSTATION';
+
